75efa436c5d3f702b9c64af32580f3ce0dcebd85
Luiz Gabriel <lgmrttl@gmail.com>
2026-06-30 16:28:51 -0300

    models

--- Back/models/Purchase.js
+++ Back/models/Purchase.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const purchaseSchema = new mongoose.Schema({
     produto: {
--- Back/models/Ranking.js
+++ Back/models/Ranking.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const rankingSchema = new mongoose.Schema({
     produto: {
