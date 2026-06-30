5a54e22e8faa218d391ae000df22a344c26fdc7c
Rhaynana Oliveira Antunes <rhaynanoliveiraantunes@gmail.com>
2026-06-30 15:26:57 -0300

    user controller creation

---
+++ Back/controllers/userController.js
@@ -0,0 +1,36 @@
+import userService from "../services/userService.js";
+
+const getMe = async (req, res, next) => {
+  try {
+    res.status(200).json(req.user);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updateMe = async (req, res, next) => {
+  try {
+    const user = await userService.updateMe(req.user._id, req.body);
+    res.json(user);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const listUsers = async (req, res, next) => {
+  try {
+    const users = await userService.listUsers();
+    res.json(users);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const toggleUserStatus = async (req, res, next) => {
+  try {
+    const user = await userService.toggleUserStatus(req.params.id);
+    res.json(user);
+  } catch (error) {
+    next(error);
+  }
+};
