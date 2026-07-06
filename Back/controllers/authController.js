9cf202cd223218bb879f25b6d9fde989114db15a
Luiz Gabriel <lgmrttl@gmail.com>
2026-07-06 14:46:25 -0300

    controllers

--- Back/controllers/authController.js
+++ Back/controllers/authController.js
@@ -3,7 +3,7 @@ import authService from "../services/authService.js";
 const register = async (req, res, next) => {
     try {
         const user = await authService.register(req.body);
-        res.status(201).json({ message: "Usuário registrado com sucesso", data: user });
+        res.status(201).json({ message: "User successfully registered", data: user });
     } catch (error) {
         next(error);
     }
@@ -12,7 +12,7 @@ const register = async (req, res, next) => {
 const login = async (req, res, next) => {
     try {
         const result = await authService.login(req.body);
-        res.status(200).json({ message: "Login realizado com sucesso", data: result });
+        res.status(200).json({ message: "Login successful", data: result });
     } catch (error) {
         next(error);
     }
